c14b1ba45ee734b0c8f986f7f71ff8d60e91a818
Tiago Royer <royertiago@gmail.com>
2015-07-08 02:58:06 -0300

    Remove debug output from thresholding.cpp.

--- thresholding.cpp
+++ thresholding.cpp
@@ -4,16 +4,12 @@
  * and everything below half with black,
  * and writes the result to stdout.
  */
-#include <iostream>
 #include "pipeimg.h"
 #include "threshold.h"
 
 int main() {
     cv::Mat img = util::in();
-
     int threshold = max(img) / 2;
-    std::clog << "Threshold: " << threshold << '\n';
-
     ::threshold( img, threshold );
     util::out( img );
     return 0;
