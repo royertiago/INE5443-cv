b9df22b5aab55a3ad16d4e0b40cc97550bf76436
Tiago Royer <royertiago@gmail.com>
2015-07-08 13:51:04 -0300

    Merge thresholding.cpp and threshold.cpp.

--- thresholding.cpp
+++ thresholding.cpp
@@ -5,7 +5,38 @@
  * and writes the result to stdout.
  */
 #include "pipeimg.h"
-#include "threshold.h"
+
+/* Returns the maximum value of c.
+ */
+int max( cv::Mat c ) {
+    int max = 0;
+    for( int i = 0; i < c.rows; i++ )
+        for( auto ptr = c.ptr<unsigned char>(i);
+                ptr != c.ptr<unsigned char>(i) + c.cols;
+                ++ptr
+            )
+        {
+            max = std::max<int>( max, *ptr );
+        }
+    return max;
+}
+
+/* Replaces every byte in the figure with either 0 or 255,
+ * according to the threshold.
+ */
+void threshold( cv::Mat img, int threshold ) {
+    for( int i = 0; i < img.rows; i++ )
+        for( auto ptr = img.ptr<unsigned char>(i);
+                ptr != img.ptr<unsigned char>(i) + img.cols;
+                ++ptr
+            )
+        {
+            if( *ptr >= threshold )
+                *ptr = 255;
+            else
+                *ptr = 0;
+        }
+}
 
 int main() {
     cv::Mat img = util::in();
