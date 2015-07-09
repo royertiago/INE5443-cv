ab112b843ce8bfa3c1d9df805f4044194f22e6b9
Tiago Royer <royertiago@gmail.com>
2015-07-08 23:05:42 -0300

    process.sh: Rewrite processing pipeline.

--- process.sh
+++ process.sh
@@ -4,5 +4,10 @@
 # preformat it in a way proper to be fed to the neural network,
 # and writes to stdout.
 format() {
-    ./thresholding | ./largest_cluster | ./resize
+    format_noresize | ./resize
+}
+
+# Same as above, but does not resize the image as the last step.
+format_noresize() {
+    ./thresholding --ratio 0.4 | ./filter --kernel h | ./largest_cluster
 }
